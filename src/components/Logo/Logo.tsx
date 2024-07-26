cd31d85cc169fe3576a12d91081cb2ec2f3c76cf
Matheus Jacoud <63015457+mjacoud@users.noreply.github.com>
2024-07-26 20:59:21 -0300

    add: Logo Component

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,10 @@
+import logo from "../../assets/logo/little-lemon-logo.png"
+import "../../App.css"
+
+const Logo = () => {
+  return (
+        <img src={logo} alt="little-lemon-logo" className="navbar__logo"/>
+  )
+}
+
+export default Logo
