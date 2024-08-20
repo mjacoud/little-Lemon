a006057bd79a178eca0d2b8bc4a882d0ef3f6e6d
Matheus Jacoud <63015457+mjacoud@users.noreply.github.com>
2024-08-19 22:40:17 -0300

    refactor(Logo): add icon type

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,19 +1,34 @@
 import logo from "../../assets/logo/little-lemon-logo.png"
 import secondary_logo from "../../assets/logo/secondary-little-lemon-logo.png"
+import icon from "../../assets/logo/little-lemon-logo-icon.png"
 
 import "../../App.css"
 import { Link } from "react-router-dom"
 
-type LogoType = "primary" | "secondary"
+type LogoType = "primary" | "secondary" | 'icon'
 
 interface ILogo{
   type:LogoType
 }
 
 const Logo: React.FC<ILogo> = ({type}) => {
+  console.log(type)
+
+  const getLogo = (type:string) => {
+    if (type == "primary"){
+      return logo
+    }
+    if (type == "secondary"){
+      return secondary_logo
+    }
+    if (type == "icon"){
+      return icon
+    }
+  }
+
   return (
         <Link to={'/'}>
-        <img src={type == 'secondary'? secondary_logo : logo } alt="little-lemon-logo" className="navbar__logo"/>
+        <img src={getLogo(type)} alt="little-lemon-logo" className={type == "icon"? 'footer__logo' : 'navbar__logo'}/>
         </Link>
   )
 }
