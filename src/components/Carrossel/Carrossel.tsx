46219c787565b92ccd843fb98bc5ab4ca8cab1f9
Matheus Jacoud <63015457+mjacoud@users.noreply.github.com>
2024-08-19 21:24:53 -0300

    feat:add Carrossel

--- src/components/Carrossel/Carrossel.tsx
+++ src/components/Carrossel/Carrossel.tsx
@@ -1,13 +1,28 @@
 import { Card } from "../Card/Card"
 import teste from '../../assets/images/mediterranian-pasta.jpg'
 
-export const Carrossel = () => {
+interface CarroselProps{
+  type:string
+}
+
+const specials = [
+  {src:teste,label:'Greek Salad',price:15,description:'The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.'}
+]
+
+const testimonials =[
+  {src:teste,label:'Greek Salad',rate:5,description:'The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.'}
+]
+
+export const Carrossel:React.FC<CarroselProps> = ({type}) => {
   return (
     <div className='flex carrossel'>
-         <Card src={teste} label='Greek Salad' price={15} description='The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.'/>
-         <Card src={teste} label='Greek Salad' price={15} description='The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.'/>
-         <Card src={teste} label='Greek Salad' price={15} description='The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.'/>
-         <Card src={teste} label='Greek Salad' price={15} description='The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.'/>
+        {type == "specials" ? specials.map(special=>
+         <Card src={special.src} type={type} label={special.label} price={special.price} description={special.description}/>
+       ) : 
+       testimonials.map(testimonial=>
+        <Card src={testimonial.src} type={type} label={testimonial.label} rate={testimonial.rate} description={testimonial.description}/>
+      )
+       } 
        </div>
   )
 }
