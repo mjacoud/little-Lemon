36f6e26606a2b7301f901fcada72b31801c1aa58
Matheus Jacoud <63015457+mjacoud@users.noreply.github.com>
2024-08-19 21:28:24 -0300

    feat:add Header Component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import Logo from "../Logo/Logo"
+import Navbar from "../Navbar/Navbar"
+
+export const Header = () => {
+  return (
+    <header className="flex flex__space-between header__container">
+        <Logo/>
+        <Navbar/>
+    </header>
+  )
+}
