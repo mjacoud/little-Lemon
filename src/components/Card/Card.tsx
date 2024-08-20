859bcd65d3d1b1f21a73970919af56b4e8157996
Matheus Jacoud <63015457+mjacoud@users.noreply.github.com>
2024-08-19 21:23:36 -0300

    feat:add Card Component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,20 +1,37 @@
 import Button from "../Button/Button"
+import { Rating } from "../Rating/Rating"
 
 interface CardProps{
   label:string
-  price:number
+  price?:number
   description:string
   src:string
+  type:string
+  rate?:number
 }
 
-export const Card:React.FC<CardProps> = ({price,description,label,src}) => {
+export const Card:React.FC<CardProps> = ({price,description,label,src,type,rate}) => {
+
+
   return (
-    <div className="background--primary card__layout">
-      <img src={src} alt={`${src}-image`} className="card__image"/>
-      <h3 className="card__title card__padding">{label}</h3>
-      <p className="card__price card__padding">U$ {price}</p>
-      <p className="card__description card__padding">{description}</p>
-      <div className="flex flex__center"><Button label="Add to Cart" type="secondary" size="small"/></div>
-    </div>
+    <>
+      { type == "specials" ?
+      <div className="background--primary card__layout">
+        <img src={src} alt={`${src}-image`} className="card__image"/>
+        <h3 className="card__title card__padding">{label}</h3>
+        <p className="card__price card__padding">U$ {price}</p>
+        <p className="card__description card__padding">{description}</p>
+        <div className="flex flex__center"><Button label="Add to Cart" type="secondary" size="small"/></div>
+      </div>
+      :
+      <div className="background--primary card__layout flex flex__center flex__column">
+        <img src={src} alt={`${src}-image`} className="card__testimonialsImage"/>
+        <h3 className="card__title card__padding">{label}</h3>
+        <Rating rate={rate!}/>
+        <p className="card__description card__padding text__center">{description}</p>
+      </div>
+        }
+    </>
+
   )
 }
