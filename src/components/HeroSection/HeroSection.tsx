d860708a85fb919e0e78970b40b1c8bc6837bf9b
Matheus Jacoud <63015457+mjacoud@users.noreply.github.com>
2024-08-23 13:44:38 -0300

    Refactor(Hero Section): fix link from Button

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -8,29 +8,33 @@ const width =useScreenWidth()
 
   return (
     <div className='background--primary flex flex__center'>
-            <Container>
-              {width <= 768 ? 
+              {width <= 767 ? 
+                <Container>
               <div className='flex flex__center flex__column'>
                 <img src={mediterranian_pasta} alt='mediterranian-pasta' className='image'/>
                 <div className='flex__center flex flex__column'>
                   <h2 className='title'>Little Lemon</h2>
                   <p className='subtitle'><i>Rio de janeiro</i></p>
                   <p className='description'>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
-                  <Button label='Reserve a Table' type='secondary' link='reserve-a-table' size='small' />
+                  <Button label='Reserve a Table' type='secondary' link='reservation' size='small' />
                 </div>
               </div> 
+            </Container>
               :
+                <Container>
               <div className='flex'>
                 <div className='flex flex__align-start flex__column padding__left-md'>
                     <h2 className='title'>Little Lemon</h2>
                     <p className='subtitle'><i>Rio de janeiro</i></p>
                     <p className='description'>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
-                    <div className='button__hero flex flex__center'><Button label='Reserve a Table' type='secondary' link='reserve-a-table' size="large"/></div>
+                    <div className='button__hero flex flex__center'>
+                      <Button label='Reserve a Table' type='secondary' link='reserve-a-table' size="large"/>
+                      </div>
                   </div>
                   <img src={mediterranian_pasta} alt='mediterranian-pasta' className='image'/>
               </div>
-              }
             </Container>
+              }
           </div>
   )
 }
