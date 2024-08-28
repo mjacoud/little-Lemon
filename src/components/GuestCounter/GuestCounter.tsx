be1badb6fe25fe671dae460e871b8d76e4bce33e
Matheus Jacoud <63015457+mjacoud@users.noreply.github.com>
2024-08-28 12:22:23 -0300

    feat:add guest counter component

--- src/components/GuestCounter/GuestCounter.tsx
+++ src/components/GuestCounter/GuestCounter.tsx
@@ -1,35 +1,51 @@
-import { useState } from "react"
+
 import plus from '../../assets/icons/plus.png'
 import minus from '../../assets/icons/minus.png'
+import { ReservationProps } from "@/pages/Reservation"
 
-export const GuestCounter = () => {
-    const [guestNumber,setGuestsNumber] = useState<number>(1)
+interface GuestCounterProps{
+  setReservation:React.Dispatch<React.SetStateAction<ReservationProps>>;
+  reservation:ReservationProps
+}
 
-    const handleGuestNumber = (operation:string) =>{
-        if (operation == "sum"){
-          if (guestNumber == 6){
-            return null
-          }
-          setGuestsNumber(prevState => prevState + 1)
+export const GuestCounter:React.FC<GuestCounterProps> = ({setReservation,reservation}) => {
+
+  const handleGuestNumber = (operation: string) => {
+    setReservation((prevState:ReservationProps) => {
+        let newGuests = prevState.guests;
+
+        if (operation === "sum") {
+            if (newGuests < 6) {
+                newGuests += 1;
+            }
+        } else if (operation === "minus") {
+            if (newGuests > 1) {
+                newGuests -= 1;
+            }
         }
-        if (operation == "minus"){
-          if (guestNumber == 1){
-            return null
-          }
-          setGuestsNumber(prevState => prevState -1)
-        }
-    }
-    
+
+        return { ...prevState, guests: newGuests };
+    });
+}
+
+      console.log(`guest number é ${reservation.guests}`)
+      console.log(reservation)
   return (
     <>
-    <div className="flex background--primary guestCounter__container flex__space-evenly">
-      <div className="background--secondary roundCorners flex flex__center" onClick={()=>handleGuestNumber('sum')}>
-        <img src={plus} alt="plus" className="icon guestCounter__icon" />
-      </div>
-      <div className="background--secondary flex flex__center guestNumber roundCorners">{guestNumber}</div>
-      <div className="background--secondary roundCorners flex flex__center" onClick={()=>handleGuestNumber('minus')}>
-      <img src={minus} alt="minus" className="icon guestCounter__icon" />
+
+    <div className="flex flex__column flex__center background--primary guestCounter__container">
+      <h3 className="reservation__title">Guest</h3>
+      <div>
+        <div className="flex background--primary guestCounter__container flex__space-evenly">
+          <div className="background--secondary roundCorners flex flex__center" onClick={()=>handleGuestNumber('minus')}>
+          <img src={minus} alt="minus" className="icon guestCounter__icon" />
         </div>
+          <div className="flex flex__center guestNumber roundCorners">{reservation.guests}</div>
+          <div className="background--secondary roundCorners flex flex__center" onClick={()=>handleGuestNumber('sum')}>
+            <img src={plus} alt="plus" className="icon guestCounter__icon" />
+          </div>
+        </div>
+      </div>
     </div>
     </>
   )
